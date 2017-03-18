d7ae938d8eafdfa70dbbaf1e034ff5de6f9a5938
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2017-03-18 15:09:45 +0100

    Category descriptor

--- src/__test__/data-manager.test.js
+++ src/__test__/data-manager.test.js
@@ -3,10 +3,56 @@ const DataManager = require('../data-manager');
 
 const dataDir = path.resolve(__dirname, 'test-data', 'section');
 
-it('Should create an entitie description object', () => {
-  const entitie = DataManager
-    .getEntitieDetails(`${dataDir}/section-one/section.md`, 'section.md');
+describe('Entitie', () => {
+  it('should match snapshoted object', () => {
+    const entitie = DataManager
+      .getEntitieDetails(
+        `${dataDir}/section-one/03212017_section.md`, '123_section.md');
 
-  expect(entitie.name).toBe('section.md');
-  expect(entitie).toMatchSnapshot();
+    expect(entitie.file.name).toBe('123_section.md');
+    expect(entitie).toMatchSnapshot();
+  });
+
+  it('should normalize date', () => {
+    const entitie = DataManager
+      .getEntitieDetails(
+        `${dataDir}/section-one/03212017_section.md`, '123_section.md');
+
+    expect(entitie.start).toBe(1490050800000);
+    expect(entitie.end).toBe(1492725600000);
+
+  });
+
+  it('should throw an error if entity name does not have a number', () => {
+    const incorrectEntitie = () => {
+      const entitie = DataManager
+        .getEntitieDetails(
+          `${dataDir}/section-one/03212017_section.md`, 'section.md');
+    };
+
+    expect(incorrectEntitie).toThrowErrorMatchingSnapshot();
+  });
+
 });
+
+it('Should create category description object', () => {
+  const category = DataManager
+    .getCategoryDetails(`${dataDir}/section-one/category-descriptor.md`);
+
+  expect(category).toMatchSnapshot();
+});
+
+it('Should traverse directories tree', () => {
+  const tree = DataManager.readDataDir(dataDir);
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('Should sort entities from biggest to lower prefix', () => {
+  const tree = DataManager.readDataDir(dataDir);
+  const [positionOne, positionTwo] = tree['section-one'].entities;
+
+  expect(positionOne.name).toBe('first-position');
+  expect(positionTwo.name).toBe('second-position');
+})
+
--- src/data-manager.js
+++ src/data-manager.js
@@ -1,15 +1,45 @@
-var marked = require('meta-marked');
-var fs = require('fs');
-var path = require('path');
+const marked = require('meta-marked');
+const fs = require('fs');
+const path = require('path');
+
+const DEFAULT_DATE_FORMAT = 'MM-YYYY';
 
 function getEntitieDetails(entitiePath, name) {
   const content = fs.readFileSync(entitiePath).toString();
   const { meta, html } = marked(content);
+  const prefix = parseInt(name.split('_')[0], 10);
+  const optionalValues = {};
 
-  return { name, meta, content: html };
+  if (isNaN(prefix)) {
+      throw new Error(`Entity prefix is not a number: ${name}`);
+  }
+
+  if (meta.start) {
+    optionalValues.start = Date.parse(meta.start);
+  }
+
+  if (meta.end) {
+    optionalValues.end = Date.parse(meta.end);
+  }
+
+  const file = {
+    name,
+    prefix,
+  };
+
+  return Object.assign({ content: html, file }, meta, optionalValues);
 }
 
-function traverseDirectoryTree(startDir) {
+function getCategoryDetails(descriptorPath) {
+  const content = fs.readFileSync(descriptorPath).toString();
+  const { meta } = marked(content);
+
+  return Object.assign({
+    dateFormat: DEFAULT_DATE_FORMAT,
+  }, meta);
+}
+
+function readDataDir(startDir) {
   const result = {};
 
   return fs
@@ -17,20 +47,23 @@ function traverseDirectoryTree(startDir) {
     .reduce((memo, item) => {
       const itemPath = path.resolve(startDir, item);
       const isMarkdownFile = path.extname(item) === '.md';
+      const isCategoryDescriptor = isMarkdownFile
+        && item.startsWith('category-descriptor');
 
-      if (isMarkdownFile) {
-        Object.assign(memo, { entities: memo.entities || [] });
-        memo.entities.push(getEntitieDetails(itemPath, item));
+      Object.assign(memo, { entities: memo.entities || [] });
+
+      if (isCategoryDescriptor) {
+        Object.assign(memo, getCategoryDetails(itemPath));
+      }
+
+      if (!isCategoryDescriptor && isMarkdownFile) {
+        memo.entities = [getEntitieDetails(itemPath, item), ...memo.entities];
       } else if (fs.statSync(itemPath).isDirectory()) {
-        memo[item] = traverseDirectoryTree(itemPath);
+        Object.assign(memo, { [item]: readDataDir(itemPath) });
       }
 
       return memo;
     }, {});
 }
 
-function readDataDir(dataDir) {
-  return traverseDirectoryTree(dataDir);
-}
-
-module.exports = { readDataDir, getEntitieDetails };
+module.exports = { readDataDir, getEntitieDetails, getCategoryDetails };
